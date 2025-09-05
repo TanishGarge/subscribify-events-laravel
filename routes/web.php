956fb9ca780423219cd9ced94f17b8d7936e78ee
Tanish Garge <=>
2025-09-06 03:22:24 +0530

    wip: Implmented a demo route to test middleware

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin', function() { return "Admin"; })->middleware(['auth','admin']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
