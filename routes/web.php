eb4d79b9926b9aafb10d1a742ed9f2b9684f81a0
Tanish Garge <=>
2025-09-07 20:15:04 +0530

    wip: Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PlanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +20,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/plan', [PlanController::class, 'index'])->name('plan');
+    Route::post('/subscribe', [SubscriptionController::class, 'store'])->name('subscribe');
+    Route::put('/subscribe/{subscription}', [SubscriptionController::class, 'update'])->name('subscribe.update');
+
+    Route::get('/events', [EventController::class, 'index'])->name('event-list');
+    Route::get('/events/{event}', [EventController::class, 'show'])->name('event-details');
 });
 
 require __DIR__.'/auth.php';
